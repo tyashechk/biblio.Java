cff91eba0d8ecd86f477a66403151b18bf9945cb
tyashechk <konarevatoma@gmail.com>
2025-10-08 14:19:44 +0300

    третьялабаЧИКЛИКИ

--- lab3.java
+++ lab3.java
@@ -14,5 +14,40 @@ public class lab3 {
                 System.out.println("y = " + y + " x = " + x);
             }
         }
+        System.out.println("------------------------------");
+        double x = -4;
+
+        while (x <= 4) {
+            y = Math.pow(x, 2) - 1;
+            System.out.println(" y = " + y + " x = " + x);
+            if (x > 0 && x <= 1.5) {
+                y = Math.cos(x);
+                System.out.println(" y = " + y + " x = " + x);
+            } else if (x > 1.5) {
+                y = Math.sin(x - 1);
+                System.out.println(" y = " + y + " x = " + x);
+            }
+            x += 0.5;
+        }
+        System.out.println("--------------------------");
+        x = -4;
+
+        do {
+            y = Math.pow(x, 2) - 1;
+            x += 0.5;
+            System.out.println(" y = " + y + " x = " + x);
+        } while (x <= 4);
+        {
+            y = Math.pow(x, 2) - 1;
+            System.out.println(" y = " + y + " x = " + x);
+            if (x > 0 && x <= 1.5) {
+                y = Math.cos(x);
+                System.out.println(" y = " + y + " x = " + x);
+            } else if (x > 1.5) {
+                y = Math.sin(x - 1);
+                System.out.println(" y = " + y + " x = " + x);
+            }
+            x += 0.5;
+        }
     }
 }
