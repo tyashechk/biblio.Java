71d010f19b50280705418dd1d3377052eb4d7424
tyashechk <konarevatoma@gmail.com>
2025-10-08 15:19:04 +0300

    ЖАВАЛАБА11111

---
+++ lab1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class lab1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in, "UTF-8");
+
+        System.out.print("Let x: ");
+        double x = scanner.nextDouble();
+        System.out.println("Your x: " + x);
+        System.out.print("Let y: ");
+        double y = scanner.nextDouble();
+        System.out.println("Your y: " + y);
+        scanner.close();
+        double pi = Math.PI;
+        double t = Math.cos(pi / 7) * Math.pow((Math.sinh(x)), 2) / ((2.7 * (x - pi)));
+        System.out.println("Finish!:" + t);
+    }
+}
